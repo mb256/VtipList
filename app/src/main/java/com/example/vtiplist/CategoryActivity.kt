9b440791c6d8aedb6e4757c946d93671e8d384a2
mb256 <mb256@gmail.com>
2023-08-03 07:42:28 +0200

    Add LazyList for 2nd activity

--- app/src/main/java/com/example/vtiplist/CategoryActivity.kt
+++ app/src/main/java/com/example/vtiplist/CategoryActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -24,83 +30,69 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.vtiplist.ui.theme.VtipListTheme
+import androidx.compose.foundation.lazy.items
 
 class CategoryActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             VtipListTheme {
-                CategoryJokeList("Empty ...")
+                JokeList(jokes)
             }
         }
     }
 }
 
+var jokes = listOf<String>(
+    "Joke 1",
+    "Joke 2",
+    "Joke 3",
+    "Joke 4",
+    "Joke 5",
+    "Joke 6",
+    "Joke 7",
+    "Joke 8",
+    "Joke 9"
+)
+
+
 @Composable
-fun CategoryJokeList(name: String, modifier: Modifier = Modifier) {
-    Column(
-        modifier = Modifier
-            //.fillMaxSize()
-            .background(Color(0xff111111)),
-        horizontalAlignment = Alignment.CenterHorizontally
+fun JokeList2(jokes: List<String>) {
+    MyUI()
+}
+
+@Composable
+fun JokeList(jokes: List<String>) {
+    Column(modifier = Modifier
+        .verticalScroll(rememberScrollState())
+        .background(Color(0xff111111))
     ) {
 
-        Headline(modifier.weight(1F))
+        Headline()
+
         Spacer(modifier = Modifier
             .height(15.dp)
             .background(Color.DarkGray))
 
-        Text(
-            text = "Some joke 1",
-            color = Color.White,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1F)
-                .background(Color.Black)
-        )
-        Spacer(modifier = Modifier
-            .height(15.dp)
-            .background(Color.DarkGray))
-        Text(
-            text = "Some joke 2",
-            color = Color.White,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1F)
-                .background(Color.Black)
-        )
-        Spacer(modifier = Modifier
-            .height(15.dp)
-            .background(Color.DarkGray))
-        Text(
-            text = "Some joke 3",
-            color = Color.White,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1F)
-                .background(Color.Black)
-        )
-        Spacer(modifier = Modifier
-            .height(15.dp)
-            .background(Color.DarkGray))
-        Text(
-            text = "Some joke 4",
-            color = Color.White,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1F)
-                .background(Color.Black)
-        )
+//        jokes.forEach { joke ->
+//            JokeRow(joke)
+//        }
+
+//        /**
+//         * import androidx.compose.foundation.lazy.items
+//         */
+//        LazyColumn {
+//            items(jokes) { joke ->
+//                JokeRow(joke)
+//            }
+//        }
+
+        MyUI()
+
+
     }
 }
+
 
 @Composable
 fun Headline(modifier: Modifier = Modifier) {
@@ -124,10 +116,59 @@ fun Headline(modifier: Modifier = Modifier) {
 }
 
 
+@Composable
+fun JokeRow(joke: String) {
+    Card(modifier = Modifier
+        .padding(all = 10.dp)
+        .fillMaxWidth()) {
+            Text(
+                text = joke,
+                color = Color.White,
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Black)
+            )
+        }
+}
+
+
+@Composable
+fun MyUI() {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(space = 8.dp)
+    ) {
+        item {
+            Text(text = "First Item")
+        }
+
+        items(count = 14) { countValue ->
+            Text(text = "Items: $countValue")
+        }
+
+        item {
+            Text(text = "Last Item")
+        }
+    }
+}
+
+/////////////////////////////////////////
+// PREVIEWS
+/////////////////////////////////////////
+
 @Preview(showBackground = true)
 @Composable
-fun CategoryJokeListPreview() {
+fun JokeListPreview() {
     VtipListTheme {
-        CategoryJokeList("Android")
+        JokeList(jokes)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun JokeList2Preview() {
+    VtipListTheme {
+        JokeList2(jokes)
     }
 }
